7788f6606cdef0109cf891722cdeeb4f64d5d94c
StrangeShadow99 <nkaley99@gmail.com>
2024-03-24 00:19:19 -0400

    Added rest

---
+++ js/playlist.js
@@ -0,0 +1,22 @@
+let songs = [
+  {
+    name: "Be Our Guest",
+    artist: "Alan Menken",
+    audio: "music1"
+  },
+  {
+    name: "Enchanted",
+    artist: "Alan Menken",
+    audio: "music2"
+  },
+  {
+    name: "Frozen",
+    artist: "Christophe Beck",
+    audio: "music3"
+  },
+  {
+    name: "A Golden Childhood",
+    artist: "Patrick Doyle",
+    audio: "music4"
+  }
+]
